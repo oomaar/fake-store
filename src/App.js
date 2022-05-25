eb5dddb37ae7b9f4afaaa779b3a4e158d441dee5
oomaar <omaar_5@hotmail.com>
2022-05-25 16:05:17 +0200

    some refactor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import { Products } from "./components";
+import { Loader, Products } from "./components";
 import { Navbar } from "./components/Navbar/Navbar";
+import useFetchData from "./hooks/useFetchData";
 
 function App() {
+  const res = useFetchData("https://fakestoreapi.com/products");
+  // console.log("🚀 ~ file: App.js ~ line 7 ~ App ~ res", res);
+
+  const { data, loading } = res;
+  console.log("🚀 ~ file: Products.js ~ line 10 ~ Products ~ loading", loading);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <>
       <Navbar />
-      <Products />
+      <Products data={data} />
     </>
   );
 }
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,18 +1,7 @@
-import useFetchData from "../../hooks/useFetchData";
-import { ProductCard, Loader } from "../";
+import { ProductCard } from "../";
 import { ProductsContainer } from "./styledProducts";
 
-export const Products = () => {
-  const res = useFetchData("https://fakestoreapi.com/products");
-  // console.log("🚀 ~ file: App.js ~ line 7 ~ App ~ res", res);
-
-  const { data, loading } = res;
-  console.log("🚀 ~ file: Products.js ~ line 10 ~ Products ~ loading", loading);
-
-  if (loading) {
-    return <Loader />;
-  }
-
+export const Products = ({ data }) => {
   const products = data.map((product, index) => (
     <ProductCard product={product} key={index} />
   ));
