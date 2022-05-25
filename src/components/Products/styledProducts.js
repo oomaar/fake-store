9b70c9f5c675e687e266471f73898fd1a5c10b5d
oomaar <omaar_5@hotmail.com>
2022-05-25 15:09:38 +0200

    style Products Component

--- src/components/Products/styledProducts.js
+++ src/components/Products/styledProducts.js
@@ -1,3 +1,8 @@
 import styled from "styled-components";
 
-export const ProductsContainer = styled.div``;
+export const ProductsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 1024px;
+  margin: 0 auto;
+`;
