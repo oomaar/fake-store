eae963405b9345bf37e2a7966359986efecfb138
oomaar <omaar_5@hotmail.com>
2022-05-25 16:05:51 +0200

    remove console.logs

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import useFetchData from "./hooks/useFetchData";
 
 function App() {
   const res = useFetchData("https://fakestoreapi.com/products");
-  // console.log("🚀 ~ file: App.js ~ line 7 ~ App ~ res", res);
 
   const { data, loading } = res;
-  console.log("🚀 ~ file: Products.js ~ line 10 ~ Products ~ loading", loading);
 
   if (loading) {
     return <Loader />;
