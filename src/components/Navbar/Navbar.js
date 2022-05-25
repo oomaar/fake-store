b3d7f6c8f794777c2727c8283e660e484e210738
oomaar <omaar_5@hotmail.com>
2022-05-25 16:23:38 +0200

    style Navbar (Responsive)

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,10 @@
-import { LogoContainer, Nav } from "./styledNavbar";
+import {
+  LogoContainer,
+  Nav,
+  SearchContainer,
+  SearchIconContainer,
+  SearchInput,
+} from "./styledNavbar";
 
 export const Navbar = () => {
   return (
@@ -6,6 +12,12 @@ export const Navbar = () => {
       <LogoContainer>
         <h1>Fake Store</h1>
       </LogoContainer>
+      <SearchContainer>
+        <SearchIconContainer>
+          <i className="bx bx-search-alt-2" />
+        </SearchIconContainer>
+        <SearchInput type="text" placeholder="Search by Product Name" />
+      </SearchContainer>
     </Nav>
   );
 };
--- src/components/Navbar/styledNavbar.js
+++ src/components/Navbar/styledNavbar.js
@@ -1,5 +1,53 @@
 import styled from "styled-components";
 
-export const Nav = styled.nav``;
+export const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  padding: 0.5rem 1rem;
+  border-bottom: 1px solid #73777b3f;
+  margin-bottom: 1rem;
+
+  @media screen and (max-width: 450px) {
+    flex-direction: column;
+    padding: 0.5rem;
+  }
+`;
 
 export const LogoContainer = styled.div``;
+
+export const SearchContainer = styled.div`
+  position: relative;
+  border: 1px solid #73777b3f;
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+  height: 30px;
+  width: 300px;
+  border-radius: 1rem;
+  padding: 0 1rem;
+
+  :focus-within {
+    border-color: #000;
+  }
+
+  @media screen and (max-width: 450px) {
+    width: 100%;
+  }
+`;
+
+export const SearchIconContainer = styled.div`
+  font-size: 1.5rem;
+  color: #73777b;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const SearchInput = styled.input`
+  width: 100%;
+  height: 100%;
+  font-size: 1rem;
+  padding-left: 0.5rem;
+  border: 0;
+  outline: 0;
+`;
