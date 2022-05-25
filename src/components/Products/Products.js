56017203d155d0ec8670e3f1190f77e9aec5adc5
oomaar <omaar_5@hotmail.com>
2022-05-25 15:38:47 +0200

    [WIP] setup a Loading state

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -6,7 +6,18 @@ export const Products = () => {
   const res = useFetchData("https://fakestoreapi.com/products");
   // console.log("🚀 ~ file: App.js ~ line 7 ~ App ~ res", res);
 
-  const products = res.map((product, index) => (
+  const { data, loading } = res;
+  console.log("🚀 ~ file: Products.js ~ line 10 ~ Products ~ loading", loading);
+
+  if (loading) {
+    return (
+      <div>
+        <h1>Loading....</h1>
+      </div>
+    );
+  }
+
+  const products = data.map((product, index) => (
     <ProductCard product={product} key={index} />
   ));
 
--- src/hooks/useFetchData.js
+++ src/hooks/useFetchData.js
@@ -3,17 +3,22 @@ import { useState, useEffect } from "react";
 
 const useFetchData = (url) => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const response = async () => {
-      await axios.get(`${url}`).then((res) => setData(res.data));
+      setLoading(true);
+      await axios.get(`${url}`).then((res) => {
+        setData(res.data);
+      });
+      setLoading(false);
     };
     response();
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return data;
+  return { data, loading };
 };
 
 export default useFetchData;
