abecc3205130eb867883e1c98ce9c9a3ca00c7a6
oomaar <omaar_5@hotmail.com>
2022-05-25 16:00:56 +0200

    define a Global Style Sheet

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import GlobleStyles from "./GlobleStyles";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <GlobleStyles />
     <App />
   </React.StrictMode>
 );
